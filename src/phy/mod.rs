234e2b98f1a403654b2d82217b8b0633c81bd06f
datdenkikniet <jcdra1@gmail.com>
2022-08-05 20:06:01 +0200

    k8zs081r -> ksz8081r

--- src/phy/mod.rs
+++ src/phy/mod.rs
@@ -7,9 +7,9 @@ pub mod lan87xxa;
 #[cfg(any(feature = "lan8720a", feature = "lan8742a"))]
 pub use lan87xxa::{LAN8720A, LAN8742A};
 
-#[cfg(feature = "kzs8081r")]
+#[cfg(feature = "ksz8081r")]
 mod ksz8081r;
-#[cfg(feature = "kzs8081r")]
+#[cfg(feature = "ksz8081r")]
 pub use ksz8081r::KSZ8081R;
 
 mod bare;
