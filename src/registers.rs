d0f9b6056ccde9c1f63e5aeb6c62f57d6444ca6c
datdenkikniet <jcdra1@gmail.com>
2026-04-25 14:31:10 +0200

    Make a list of regs

--- src/registers.rs
+++ src/registers.rs
@@ -2,8 +2,34 @@
 //!
 //! These register definitions are based on IEEE 802.3-2022
 
+// 0x0: basic
+// 0x1: basic
+// 0x2: see lib.rs
+// 0x3: see lib.rs
+// 0x4: auto_negotiation
+// 0x5: auto_negotation
+// 0x6: auto_negotation
+// 0x7: N/A
+// 0x8: N/A
+// 0x9: leader_follower
+// 0xa: leader_follower
+// 0xb: N/A
+// 0xc: N/A
+// 0xd: MMD
+// 0xe: MMD
+// 0xf: basic
+
+// Reg 2 and 3 are PHY ident and are handled at a higher
+// level.
+//
+// 12 and 13 are MMD registers
+
+// 4, 5, 6
+// 7 and 8 not handled yet
 pub mod auto_negotiation;
+// 0, 1 and 15
 mod basic;
+// 9 and 10
 pub mod leader_follower;
 
 pub use basic::{
