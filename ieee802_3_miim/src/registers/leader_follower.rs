3e36c928d630de8ac335f18109b2ebd5e7b4dd6b
datdenkikniet <jcdra1@gmail.com>
2026-07-16 21:19:08 +0200

    convert registers to bitbybit

--- ieee802_3_miim/src/registers/leader_follower.rs
+++ ieee802_3_miim/src/registers/leader_follower.rs
@@ -1,12 +1,12 @@
 //! Leader-follower registers.
 
-use bilge::{bitsize, prelude::*, FromBits};
+use arbitrary_int::u3;
 
 use crate::registers::{Register, RegisterAddress};
 
 /// The resolved or preference of port type.
-#[bitsize(1)]
-#[derive(Clone, Copy, FromBits, Debug, PartialEq)]
+#[bitbybit::bitenum(u1, exhaustive = true)]
+#[derive(Debug, PartialEq, Eq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum PortType {
     /// The port is a leader.
@@ -21,30 +21,36 @@ pub enum PortType {
 /// bits, as well as configuration to be used during LEADER-FOLLOWER
 /// negotiation.
 ///
-/// Defined in 40.5.1.1.
-#[bitsize(16)]
-#[derive(Clone, Copy, FromBits, DebugBits)]
-#[cfg_attr(feature = "defmt", derive(bilge_defmt::FormatBits))]
+// Defined in 40.5.1.1.
+#[bitbybit::bitfield(u16, forbid_overlaps, default = 0, defmt_bitfields(feature = "defmt"))]
+#[derive(Debug, PartialEq)]
 pub struct LeaderFollowerControl {
-    reserved: u8,
     /// Advertise 1000BASE-T Half Duplex during
     /// autonegotiation.
+    #[bit(8, rw)]
     pub _1000base_t_hd: bool,
     /// Advertise 1000BASE-T Full Duplex during
     /// autonegotiation.
+    #[bit(9, rw)]
     pub _1000base_t_fd: bool,
     /// If `!manual_config`, indicate the type of port that
     /// this device should prefer to be.
+    #[bit(10, rw)]
     pub port_type_preference: PortType,
     /// If `manual_config`, set the port type that this device
     /// should use during negotiation.
+    #[bit(11, rw)]
     pub config_value: PortType,
     /// Whether manual or LEADER-FOLLOWER negotiation should be
     /// used to determine the [`PortType`] of this device.
+    #[bit(12, rw)]
     pub manual_config: bool,
     /// The test mode bits.
+    #[bits(13..=15, rw)]
     pub test_mode: u3,
 }
+
+from_into!(LeaderFollowerControl);
 
 impl Register for LeaderFollowerControl {
     const ADDRESS: RegisterAddress = RegisterAddress::new(9).unwrap();
@@ -56,35 +62,42 @@ impl Register for LeaderFollowerControl {
 /// bits, as well as status resolved during LEADER-FOLLOWER
 /// negotiation.
 ///
-/// Defined in 40.5.1.1.
-#[bitsize(16)]
-#[derive(Clone, Copy, FromBits, DebugBits)]
-#[cfg_attr(feature = "defmt", derive(bilge_defmt::FormatBits))]
+// Defined in 40.5.1.1.
+#[bitbybit::bitfield(u16, forbid_overlaps, default = 0, defmt_bitfields(feature = "defmt"))]
+#[derive(Debug, PartialEq)]
 pub struct LeaderFollowerStatus {
     /// The cumulative count of errors detected when the
     /// receiver is receiving idles.
     ///
     /// This field is reset to zero when the register is read.
+    #[bits(0..=7, rw)]
     pub idle_error_count: u8,
-    reserved: u2,
     /// Link partner advertises 1000BASE-T Half Duplex.
+    #[bit(10, rw)]
     pub _1000base_t_hd: bool,
     /// Link partner advertises 1000BASE-T Full Duplex.
+    #[bit(11, rw)]
     pub _1000base_t_fd: bool,
     /// Whether the remote receiver is OK, i.e. whether reception
     /// is detected to be reliable for our link partner.
+    #[bit(12, rw)]
     pub remote_receiver_ok: bool,
     /// Whether the local receiver is OK, i.e. whether reception
     /// is detected to be reliable for this PHY.
+    #[bit(13, rw)]
     pub local_receiver_ok: bool,
     /// The configuration that the local PHY resolved to.
+    #[bit(14, rw)]
     pub leader_follower_resolution: PortType,
     /// The combination of the local [`LeaderFollowerControl::config_value`] and
     /// the remote [`LeaderFollowerControl::config_value`] is invalid.
     ///
     /// This field is cleared when the register is read.
+    #[bit(15, rw)]
     pub leader_follower_config_fault: bool,
 }
+
+from_into!(LeaderFollowerStatus);
 
 impl Register for LeaderFollowerStatus {
     const ADDRESS: RegisterAddress = RegisterAddress::new(10).unwrap();
